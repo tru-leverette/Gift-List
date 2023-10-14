9c512358393f26ad8240ed7135b76ba9637c3d6f
User <user@Users-MacBook-Pro.local>
2023-10-14 12:37:31 -0500

    feat: initial commit, added homw page, added routing

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import './home.scss'
+
+
+
+
+const Home = () => {
+
+    return(
+        <div className="home-container">Home</div>
+    )
+}
+
+
+export default Home
